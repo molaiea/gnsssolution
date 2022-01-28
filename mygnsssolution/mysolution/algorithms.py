c46205959462d87ff181e895709dafa55fd6966a
molaiea <nafissaatlagh1809@gmail.com>
2022-01-28 15:03:11 +0100

    adding algorithms and stuff hehe

--- mygnsssolution/mysolution/algorithms.py
+++ mygnsssolution/mysolution/algorithms.py
@@ -1,32 +1,35 @@
 import georinex as gr
-from math import sin, abs, atan, sqrt, cos
+from math import sin, atan, sqrt, cos
 import numpy as np
 
+def extract_params(file_name, sat_name, epoch):
+    nav = gr.load(file_name)
+    M0 = nav.sel(sv=sat_name)['M0'].values[epoch]
+    e = nav.sel(sv=sat_name)['Eccentricity'].values[epoch]
+    sqrtA = nav.sel(sv=sat_name)['sqrtA'].values[epoch]
+    Omega0 = nav.sel(sv=sat_name)['Omega0'].values[epoch]
+    Io = nav.sel(sv=sat_name)['Io'].values[epoch]
+    omega = nav.sel(sv=sat_name)['omega'].values[epoch]
+    DeltaN = nav.sel(sv=sat_name)['DeltaN'].values[epoch]
+    cuc = nav.sel(sv=sat_name)['Cuc'].values[epoch]
+    cus = nav.sel(sv=sat_name)['Cus'].values[epoch]
+    crc = nav.sel(sv=sat_name)['Crc'].values[epoch]
+    crs = nav.sel(sv=sat_name)['Crs'].values[epoch]
+    cis = nav.sel(sv=sat_name)['Cis'].values[epoch]
+    cic = nav.sel(sv=sat_name)['Cic'].values[epoch]
+    OmegaDot = nav.sel(sv=sat_name)['OmegaDot'].values[epoch]
+    IDOT = nav.sel(sv=sat_name)['IDOT'].values[epoch]
+    toe = nav.sel(sv=sat_name)['Toe'].values[epoch]
+    t_h = float(str(nav.sel(sv='G08').coords['time'][epoch].values).split('T')[1].split(':')[0])*3600
+    t_min = float(str(nav.sel(sv='G08').coords['time'][epoch].values).split('T')[1].split(':')[1])*60
+    t_s = float(str(nav.sel(sv='G08').coords['time'][epoch].values).split('T')[1].split(':')[2])
+    t = t_h + t_min + t_s
+    return M0, e, sqrtA, Omega0, Io, omega, DeltaN, cuc, cus, crc, crs, cis, cic, OmegaDot, IDOT, toe, t
 
-def satellite_position(file_name, sat_name):
+def satellite_position(file_name, sat_name, epoch):
     #Extraction des paramètes
     #--------------------------------------------------
-    nav = gr.load(file_name)
-    M0 = nav.sel(sv=sat_name)['M0']
-    e = nav.sel(sv=sat_name)['Eccentricity']
-    sqrtA = nav.sel(sv=sat_name)['sqrtA']
-    Omega0 = nav.sel(sv=sat_name)['Omega0']
-    Io = nav.sel(sv=sat_name)['Io']
-    omega = nav.sel(sv=sat_name)['omega']
-    DeltaN = nav.sel(sv=sat_name)['DeltaN']
-    cuc = nav.sel(sv=sat_name)['Cuc']
-    cus = nav.sel(sv=sat_name)['Cus']
-    crc = nav.sel(sv=sat_name)['Crc']
-    crs = nav.sel(sv=sat_name)['Crs']
-    cis = nav.sel(sv=sat_name)['Cis']
-    cic = nav.sel(sv=sat_name)['Cic']
-    OmegaDot = nav.sel(sv=sat_name)['OmegaDot']
-    IDOT = nav.sel(sv=sat_name)['IDOT']
-    toe = nav.sel(sv=sat_name)['Toe']
-    t_h = int(str(nav.sel(sv='G08').coords['time'][0].values).split('T')[1].split(':')[0])*3600
-    t_min = int(str(nav.sel(sv='G08').coords['time'][0].values).split('T')[1].split(':')[1])*60
-    t_s = int(str(nav.sel(sv='G08').coords['time'][0].values).split('T')[1].split(':')[2])
-    t = t_h + t_min + t_s
+    M0, e, sqrtA, Omega0, Io, omega, DeltaN, cuc, cus, crc, crs, cis, cic, OmegaDot, IDOT, toe, t = extract_params(file_name, sat_name, epoch)
     #--------------------------------------------------
     #Calculs
     t_k = t-toe
@@ -42,7 +45,7 @@ def satellite_position(file_name, sat_name):
     E_k = E0 + e*sin(E0)
     while abs(E0-E_k)<0.000001 :
         E0 = E_k
-        E_k = E0 + e*sin(E0)
+        E_k = M_k + e*sin(E0)
     
     #Calcul de l'anomalie vraie
     v_k = atan((sqrt(1-e**2)*sin(E_k))/(cos(E_k)-e))
@@ -64,9 +67,39 @@ def satellite_position(file_name, sat_name):
     coords = np.dot(dotproduct, vector)
     return coords
 
+# print(satellite_position("rabt3320.19n","G08",0))
 
+def app_coords(X, Y, Z, D):
+    # X Y Z D are respectively arrays of 4 coordinates and distances of the first 4 satellites, their purpose is to calculate the approximate coords 
+    B=np.array([[X[0]-X[1],Y[0]-Y[1],Z[0]-Z[1]],[X[1]-X[2],Y[1]-Y[2],Z[1]-Z[2]],[X[2]-X[3],Y[2]-Y[3],Z[2]-Z[3]]])
+    C=np.array([[D[1]**2-D[0]**2-(X[1]**2+Y[1]**2+Z[1]**2)+(X[0]**2+Y[0]**2+Z[0]**2)],[D[2]**2-D[1]**2-(X[2]**2+Y[2]**2+Z[2]**2)+(X[1]**2+Y[1]**2+Z[1]**2)],[D[3]**2-D[2]**2-(X[3]**2+Y[3]**2+Z[3]**2)+(X[2]**2+Y[2]**2+Z[2]**2)]])*0.5
+    X0=np.dot(np.linalg.inv(B),C)
+    return X0
 
+# X0 = app_coords(X,Y,Z,D)
 
+# Xa = np.array((221122,112233,221113,312342,661122,332233,991113,392342))
+# Ya = np.array((621122,712233,621113,712342,721122,912233,821113,212342))
+# Za = np.array((921122,812233,921113,912342,821122,512233,621113,412342))
+# Da = np.array((21121122,11222233,22331113,31442342,28921122,15222233,22661113,31322342))
+def jacobienne(X, Y, Z, D, X0):
+    # X Y Z D are respectively arrays of all coordinates and distances of the all the satellites, X0 are the app coords of the station
+    A=np.zeros((len(X), 3), dtype=float)
+    W=np.zeros((len(X), 1), dtype=float)
+    for i in range(len(D)):
+        E=sqrt((X0[0][0]-X[i])**2+(X0[1][0]-Y[i])**2+(X0[2][0]-Z[i])**2)
+        A[i][0]=(X0[0][0]-X[i])/E
+        A[i][1]=(X0[1][0]-Y[i])/E
+        A[i][2]=(X0[2][0]-Z[i])/E
+        W[i][0]=E-D[i]
+    return A, W
+# A, W = jacobienne(Xa, Ya, Za, Da, X0)
 
+def minimos_cuadrados(A, W, X0):
+    N=np.dot(A.transpose(),A)
+    U=np.dot(A.transpose(),W)
+    X=np.dot(np.linalg.inv(N),U)
+    Xcorr=X0-X
+    return Xcorr
 
 
--- mygnsssolution/mysolution/views.py
+++ mygnsssolution/mysolution/views.py
@@ -4,23 +4,53 @@ import georinex as gr
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import UploadFileForm
-
+import numpy as np
+from .algorithms import satellite_position, app_coords, jacobienne, minimos_cuadrados, extract_params
 # Imaginary function to handle an uploaded file.
 #from somewhere import handle_uploaded_file
 def handle_uploaded_file(f):
-    with open('name.txt', 'wb+') as destination:
+    name = str(f)
+    cachepath = "D:\gnsssolution\mygnsssolution\mysolution\cache"
+    path = f"{cachepath}\{name}"
+    with open(path , 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    nav = gr.load(path)
+    epoch1 = nav.coords['time'].values[0]
+    epoch2 = nav.coords['time'].values[1]
+    satellites = nav.coords['sv'].values
+    values_epoch1 = np.zeros((len(satellites),17))
+    satellites_in_range_epoch1 = []
+    satellites_in_range_epoch2 = []
+    values_epoch2 = np.zeros((len(satellites),17))
+    for i in range(len(satellites)):
+        values_epoch1[i] = np.array((extract_params(path, satellites[i], 0)))
+        values_epoch2[i] = np.array((extract_params(path, satellites[i], 1)))
+        if len(values_epoch1[i][~np.isnan(values_epoch1[i])]) > 1:
+            satellites_in_range_epoch1.append(satellites[i])
+        if len(values_epoch2[i][~np.isnan(values_epoch2[i])]) > 1:
+            satellites_in_range_epoch2.append(satellites[i])
+    print(satellites_in_range_epoch1)
+    coords_epoch1 = {}
+    coords_epoch2 = {}
+    for i in range(len(satellites_in_range_epoch1)):
+        coords_epoch1[satellites_in_range_epoch1[i]] = satellite_position(path,satellites_in_range_epoch1[i],0) 
+    for i in range(len(satellites_in_range_epoch2)):
+        coords_epoch2[satellites_in_range_epoch2[i]] = satellite_position(path,satellites_in_range_epoch2[i],1) 
+    
+    print(coords_epoch1)
+    print(coords_epoch2)
+
 
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            print(request.FILES['file'])
+            print(str(request.FILES['file']))
             handle_uploaded_file(request.FILES['file'])
-            nav = gr.load('name.txt')
-            print(nav.sel(sv='G08')['SVclockBias'])
+            # nav = gr.load('name.txt')
+            # print(nav.sel(sv='G08')['SVclockBias'])
 
             return HttpResponseRedirect('/upload')
     else:
